51303b5d79b77038d7a19635b2dbcd22260a68ac
Jan von der Assen <jvdassen@dizmo.com>
2016-04-23 23:03:33 +0200

    add styling to tiles

--- build/tictactoe/application.js
+++ build/tictactoe/application.js
@@ -7,9 +7,9 @@ function showBack() {
 function checkWinner(sign){
 	var won = false;
 
-	if (jQuery('#ff').text() === sign){
-		if (jQuery('#fs').text() === sign){
-			if (jQuery('#ft').text() === sign){
+	if (jQuery('#ff').attr('clicked') === sign){
+		if (jQuery('#fs').attr('clicked') === sign){
+			if (jQuery('#ft').attr('clicked') === sign){
 				won = true
 			}	
 		}
@@ -18,9 +18,21 @@ function checkWinner(sign){
 		console.log("not ", sign);
 	}
 
-	if (jQuery('#sf').text() === sign){
-		if (jQuery('#ss').text() === sign){
-			if (jQuery('#st').text() === sign){
+	if (jQuery('#sf').attr('clicked') === sign){
+		if (jQuery('#ss').attr('clicked') === sign){
+			if (jQuery('#st').attr('clicked') === sign){
+				won = true
+			}	
+		}
+	}
+
+	else {
+		console.log("not", sign);
+	}	
+
+	if (jQuery('#tf').attr('clicked') === sign){
+		if (jQuery('#ts').attr('clicked') === sign){
+			if (jQuery('#tt').attr('clicked') === sign){
 				won = true
 			}	
 		}
@@ -29,9 +41,10 @@ function checkWinner(sign){
 		console.log("not", sign);
 	}	
 
-	if (jQuery('#tf').text() === sign){
-		if (jQuery('#ts').text() === sign){
-			if (jQuery('#tt').text() === sign){
+
+	if (jQuery('#ff').attr('clicked') === sign){
+		if (jQuery('#sf').attr('clicked') === sign){
+			if (jQuery('#tf').attr('clicked') === sign){
 				won = true
 			}	
 		}
@@ -40,21 +53,9 @@ function checkWinner(sign){
 		console.log("not", sign);
 	}	
 
-
-	if (jQuery('#ff').text() === sign){
-		if (jQuery('#sf').text() === sign){
-			if (jQuery('#tf').text() === sign){
-				won = true
-			}	
-		}
-	}
-	else {
-		console.log("not", sign);
-	}	
-
-	if (jQuery('#fs').text() === sign){
-		if (jQuery('#ss').text() === sign){
-			if (jQuery('#ts').text() === sign){
+	if (jQuery('#fs').attr('clicked') === sign){
+		if (jQuery('#ss').attr('clicked') === sign){
+			if (jQuery('#ts').attr('clicked') === sign){
 				won = true
 			}	
 		}
@@ -63,23 +64,23 @@ function checkWinner(sign){
 		console.log("not", sign);
 	}
 	
-	if (jQuery('#ft').text() === sign){
-		if (jQuery('#st').text() === sign){
-			if (jQuery('#tt').text() === sign){
+	if (jQuery('#ft').attr('clicked') === sign){
+		if (jQuery('#st').attr('clicked') === sign){
+			if (jQuery('#tt').attr('clicked') === sign){
 				won = true
 			}	
 		}
 	}
-	if (jQuery('#ff').text() === sign){
-		if (jQuery('#ss').text() === sign){
-			if (jQuery('#tt').text() === sign){
+	if (jQuery('#ff').attr('clicked') === sign){
+		if (jQuery('#ss').attr('clicked') === sign){
+			if (jQuery('#tt').attr('clicked') === sign){
 				won = true
 			}	
 		}
 	}
-	if (jQuery('#ft').text() === sign){
-		if (jQuery('#ss').text() === sign){
-			if (jQuery('#tf').text() === sign){
+	if (jQuery('#ft').attr('clicked') === sign){
+		if (jQuery('#ss').attr('clicked') === sign){
+			if (jQuery('#tf').attr('clicked') === sign){
 				won = true
 			}	
 		}
@@ -96,6 +97,8 @@ function checkWinner(sign){
 // As soon as the dom is loaded, and the dizmo is ready, instantiate the main class
 window.document.addEventListener('dizmoready', function() {
     // Your code should be in here so that it is secured that the dizmo is fully loaded
+    	dizmo.setWidth(190);
+
     document.getElementById('doneBtn').onclick = function() {
         dizmo.showFront();
         dizmo.setWidth(190);
@@ -103,27 +106,34 @@ window.document.addEventListener('dizmoready', function() {
 
     jQuery('.tile').click(function (){
     	
-    	if (jQuery('#' + jQuery(this).attr('id')).text() === "" & playerturn != 0){
+    	if (jQuery('#' + jQuery(this).attr('id')).attr('clicked') === "" & playerturn != 0){
 
     		if(playerturn === 1) {
-    			jQuery('#' + jQuery(this).attr('id')).text("X");
+    			jQuery('#' + jQuery(this).attr('id')).attr('clicked', 'X');
+    			// jQuery('#' + jQuery(this).attr('id')).text('X');
+
+				jQuery('#' + jQuery(this).attr('id') + ' .xoro').text('X');
     			
+
     			if (checkWinner("X")){
     				console.log("won: "+ playerturn);
+    				$('#winner').text('Player ' + playerturn + ' won!');
     				playerturn = 0;
     			}
     			
     			else {
-
     				playerturn = 2;
     			}
     		}
 
     		else {
-    			jQuery('#' + jQuery(this).attr('id')).text("O");
+    			jQuery('#' + jQuery(this).attr('id')).attr('clicked', 'O');
+				jQuery('#' + jQuery(this).attr('id') + ' .xoro').text('O');
+
     			 if (checkWinner("O")){
     				console.log("won: "+ playerturn);
-    				playerturn = 0;
+    				$('#winner').text('Player ' + playerturn + ' won!');
+    				layerturn = 0;
     			}
     			
     			else {
